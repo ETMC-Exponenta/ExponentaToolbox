1dff2d2b532cff7e56f5f12e5e61f31de77e1024
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-16 19:37:53 +0300

    Improve notifier

--- ExponentaNotifier.m
+++ ExponentaNotifier.m
@@ -21,72 +21,61 @@ classdef ExponentaNotifier < handle
             obj.Updater = ExponentaUpdater();
             obj.Name = obj.Updater.ext.name;
             obj.Offline = ~obj.Updater.isonline();
+            obj.Data = obj.readNotifications();
             if nargin > 0
                 obj.GetDataCallback = cbfun;
             end
-            Async(@(~)obj.getData, 2);
+            obj.updateNotifications();
+            Async(@(~)obj.downloadNotifications, 2);
         end
         
-        function getData(obj)
+        function downloadNotifications(obj)
             %% Get notifications data
             furl = obj.Updater.ext.getrawurl(obj.DataPath);
+            data = [];
             if ~obj.Offline
                 try
                     txt = webread(furl);
-                    obj.Data = struct2table(jsondecode(txt), 'AsArray', true);
+                    data = struct2table(jsondecode(txt), 'AsArray', true);
                 catch
                     obj.Offline = true;
                 end
             end
-            obj.getNotifications();
+            if ~obj.Offline
+                if isempty(data)
+                    obj.Data = [];
+                else
+                    if isempty(obj.Data)
+                        obj.Data = data;
+                        obj.Data.read = false(height(data), 1);
+                    else
+                        obj.Data = innerjoin(data, obj.Data(:, {'code' 'read'}), 'Keys', 'code');
+                    end
+                end
+                obj.updateNotifications();
+                obj.setIcon();
+            end
+        end
+        
+        function updateNotifications(obj)
+            %% Update notifications to actual date
+            N = obj.Data;
+            if ~isempty(N)
+                obj.Data = N(N.duedate >= datetime('today'), :);
+                obj.saveNotifications();
+            end
             if ~isempty(obj.GetDataCallback)
                 obj.GetDataCallback(obj.Data);
             end
         end
         
-        function setIcon(obj)
-            %% Set shortcut icon
-            if obj.Updater.ext.isfav(obj.NotifierName)
-                favs = com.mathworks.mlwidgets.favoritecommands.FavoriteCommands.getInstance();
-                c0 = favs.getCommandProperties(obj.NotifierName, obj.Name);
-                c1 = favs.getCommandProperties(obj.NotifierName, obj.Name);
-                if isempty(obj.Data) || all(obj.Data.read)
-                    c1.setIconName(obj.IconDefault);
-                else
-                    c1.setIconName(obj.IconNotify);
-                end
-                favs.updateCommand(c0, c1);
-            end
-        end
-        
-        function N = getNotifications(obj)
-            %% Get notifications
-            if ~obj.Offline
-                if ~isempty(obj.Data)
-                    issaved = ispref(obj.Name, obj.Key);
-                    if issaved
-                        N = getpref(obj.Name, obj.Key);
-                    else
-                        N = [];
-                    end
-                    if isempty(N)
-                        N = obj.Data;
-                        N.read = false(height(obj.Data), 1);
-                    else
-                        N = innerjoin(obj.Data, N(:, {'code' 'read'}), 'Keys', 'code');
-                    end
-                else
-                    N = [];
-                end
-            else
+        function N = readNotifications(obj)
+            %% Read saved notifications
+            if ispref(obj.Name, obj.Key)
                 N = getpref(obj.Name, obj.Key);
+            else
+                N = [];
             end
-            if ~isempty(N)
-                obj.Data = N(N.duedate >= datetime('today'), :);
-            end
-            obj.Data = N;
-            obj.saveNotifications();
-            obj.setIcon();
         end
         
         function saveNotifications(obj)
@@ -115,6 +104,21 @@ classdef ExponentaNotifier < handle
                 num = nnz(~obj.Data.read);
             else
                 num = 0;
+            end
+        end
+        
+        function setIcon(obj)
+            %% Set shortcut icon
+            if obj.Updater.ext.isfav(obj.NotifierName)
+                favs = com.mathworks.mlwidgets.favoritecommands.FavoriteCommands.getInstance();
+                c0 = favs.getCommandProperties(obj.NotifierName, obj.Name);
+                c1 = favs.getCommandProperties(obj.NotifierName, obj.Name);
+                if isempty(obj.Data) || all(obj.Data.read)
+                    c1.setIconName(obj.IconDefault);
+                else
+                    c1.setIconName(obj.IconNotify);
+                end
+                favs.updateCommand(c0, c1);
             end
         end
         
