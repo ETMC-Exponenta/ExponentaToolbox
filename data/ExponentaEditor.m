0a555d80f2bcdd55c3945dc2c5e711c2655f1a19
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-19 18:51:04 +0300

    Exponenta Editor started

---
+++ data/ExponentaEditor.m
@@ -0,0 +1,71 @@
+classdef ExponentaEditor < handle
+    %EXPONENTAEDITOR Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Root
+        FileName = 'notifications.json'
+        Data
+    end
+    
+    methods
+        function obj = ExponentaEditor()
+            %% Constructor
+            obj.Root = fileparts(mfilename('fullpath'));
+            obj.read();
+        end
+        
+        function data = read(obj)
+            %% Read data
+            data = obj.json_read(fullfile(obj.Root, obj.FileName), true);
+            data.duedate = datetime(data.duedate);
+            obj.Data = data;
+        end
+        
+        function write(obj)
+            %% Write data
+            obj.json_write(fullfile(obj.Root, obj.FileName), obj.Data, true);
+        end
+        
+        function code = genCode(~)
+            %% Generate new code
+            code = datestr(datetime, 'yymmddHHMMss');
+        end
+        
+        function data = json_read(~, fname, asTable)
+            %% Read data from .json file
+            if nargin < 3
+                asTable = true;
+            end
+            data = loadjson(fname, 'Encoding', 'UTF-8');
+            if asTable && ~(isstruct(data) && isscalar(data))
+                data = struct2table(vertcat(data{:}), 'AsArray', true);
+            end
+        end
+        
+        function json_write(~, fname, data, asArray)
+            %% Write data to .json file
+            if nargin < 4
+                asArray = true;
+            end
+            if istable(data)
+                data = reshape(table2struct(data), 1, []);
+            end
+            if asArray
+                data = arrayfun(@(x) {x}, data);
+            end
+            savejson('', data, 'FileName', fname, 'ParseLogical', 1, 'Encoding', 'UTF-8');
+        end
+        
+        function check_jsonlab(~)
+            %% Check jsonlab in installed
+            w1 = which('loadjson');
+            w2 = which('savejson');
+            if isempty(w1) || isempty(w2)
+                error('Install <a href="https://github.com/fangq/jsonlab">jsonlab</a> first');
+            end
+        end
+        
+    end
+end
+
---
+++ data/installExponentaEditor.m
@@ -0,0 +1,6 @@
+function installExponentaEditor()
+%INSTALLEXPONENTAEDITOR Summary of this function goes here
+%   Detailed explanation goes here
+
+end
+
