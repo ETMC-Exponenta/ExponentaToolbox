f0ba965d42c785783e4618822252f628558fe36c
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-22 18:45:05 +0300

    Оптимизировал веб-установщик под старые релизы

--- installweb.m
+++ installweb.m
@@ -5,8 +5,18 @@ instRes = webread(instURL);
 fprintf('Downloading %s %s\n', instName, instRes.name);
 websave(instRes.assets.name, instRes.assets.browser_download_url);
 disp('Installing...')
-open(instRes.assets.name);
+v = version;
+if contains(v, {'R2018' 'R2017' 'R2016' 'R2015'})
+    installAppDesignerPro();
+    matlab.addons.install(instRes.assets.name);
+else
+    open(instRes.assets.name);
+end
 clear instURL instRes instName
 disp('Installation complete!')
 % Post-install commands
 % Add your post-install commands below
+
+function installAppDesignerPro()
+eval(webread('https://git.io/fjbyL')); % Install App Designer Pro Library
+end
