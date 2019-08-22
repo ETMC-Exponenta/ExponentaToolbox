183e517708dfbcbc55c5950576912db482f41899
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-22 18:47:15 +0300

    Исправил веб-установщик

--- installweb.m
+++ installweb.m
@@ -1,4 +1,8 @@
 % Generated with Toolbox Extender https://github.com/ETMC-Exponenta/ToolboxExtender
+instOld = contains(version, {'R2018' 'R2017' 'R2016' 'R2015'});
+if instOld
+    eval(webread('https://git.io/fjbyL')); % Install App Designer Pro Library
+end
 instURL = 'https://api.github.com/repos/ETMC-Exponenta/ExponentaToolbox/releases/latest';
 [~, instName] = fileparts(fileparts(fileparts(instURL)));
 instRes = webread(instURL);
@@ -6,17 +10,12 @@ fprintf('Downloading %s %s\n', instName, instRes.name);
 websave(instRes.assets.name, instRes.assets.browser_download_url);
 disp('Installing...')
 v = version;
-if contains(v, {'R2018' 'R2017' 'R2016' 'R2015'})
-    installAppDesignerPro();
+if instOld
     matlab.addons.install(instRes.assets.name);
 else
     open(instRes.assets.name);
 end
-clear instURL instRes instName
+clear instURL instRes instName instOld
 disp('Installation complete!')
 % Post-install commands
 % Add your post-install commands below
-
-function installAppDesignerPro()
-eval(webread('https://git.io/fjbyL')); % Install App Designer Pro Library
-end
