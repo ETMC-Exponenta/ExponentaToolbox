817c3fd835dbea7451234f09df458fd33ebd8861
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-22 18:55:38 +0300

    Обновлены установщик и проект

--- install.m
+++ install.m
@@ -1,5 +1,8 @@
 function install
 % Generated with Toolbox Extender https://github.com/ETMC-Exponenta/ToolboxExtender
+if contains(version, {'R2018' 'R2017' 'R2016' 'R2015'})
+    eval(webread('https://git.io/fjbyL')); % Install App Designer Pro Library
+end
 dev = ExponentaDev;
 dev.test('', false);
 % Post-install commands
