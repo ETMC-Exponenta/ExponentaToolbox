a9d4229ffaa4be94423038cc83b3251868275d95
Pavel Roslovets <roslovets@exponenta.ru>
2019-08-22 13:47:14 +0300

    v0.3

--- installweb.m
+++ installweb.m
@@ -5,7 +5,7 @@ instRes = webread(instURL);
 fprintf('Downloading %s %s\n', instName, instRes.name);
 websave(instRes.assets.name, instRes.assets.browser_download_url);
 disp('Installing...')
-matlab.addons.install(instRes.assets.name);
+open(instRes.assets.name);
 clear instURL instRes instName
 disp('Installation complete!')
 % Post-install commands
